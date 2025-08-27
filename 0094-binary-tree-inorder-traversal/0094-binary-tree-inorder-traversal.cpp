df904659b2a8d8d2a7180fcb879ffd07ac011eb7
Tarandeep Singh. <121600765+tsj2003@users.noreply.github.com>
2025-08-27 11:32:59 +0530

    Time: 0 ms (100%), Space: 10.6 MB (98.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,22 +10,36 @@
  * };
  */
 class Solution {
-public:   
-void helper(TreeNode* root, vector<int> &ans ){
-
-    if(root == NULL){
-        return;
-    }
-
-    helper(root->left , ans);
-    ans.push_back(root->val);
-    helper(root->right , ans);
-}
-
-
+public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        helper(root , ans);
-        return ans;
+
+         vector<int> ans;
+         TreeNode* curr = root;
+         while(curr!=NULL){
+
+           
+
+                if(curr->left == NULL){
+
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+
+                }
+                else{
+                    TreeNode* leftchild = curr->left;
+                    while(leftchild->right !=  NULL){
+                        leftchild = leftchild->right;
+                    }
+                    leftchild->right = curr;
+
+                    TreeNode* temp = curr;
+                    curr = curr->left;
+                    temp->left = NULL;
+
+                }
+            }
+         
+         return ans;
+
     }
 };
