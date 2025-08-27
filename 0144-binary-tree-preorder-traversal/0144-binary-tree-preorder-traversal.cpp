90d5ba63fca3910bcc8d64bc81f6bf389bb05285
Tarandeep Singh. <121600765+tsj2003@users.noreply.github.com>
2025-08-27 14:43:09 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.56%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,21 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void pre(TreeNode* root ,vector<int> &ans ){
-        if(root == nullptr){
-            return;
-
-        }
-        ans.push_back(root->val);
-        pre(root->left , ans);
-        pre(root->right,ans);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        pre(root , ans);
-        return ans;
-        
+        TreeNode* curr = root;
+        while(curr!=NULL){
+
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+                
+
+            }
+            else{
+                   TreeNode* leftchild = curr->left;
+
+              while(leftchild->right !=NULL  && leftchild->right != curr){
+
+                leftchild = leftchild->right;
+              }
+              if(leftchild->right == NULL){
+               ans.push_back(curr->val); 
+                leftchild->right = curr;
+                curr = curr->left;
+
+
+              }
+              else{
+                leftchild->right = NULL;
+                
+                curr = curr->right;
+              }
+
+
+            }
+        }
+        return ans; 
     }
 };
